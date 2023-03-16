f136fffa90c40c1fc7d836c02ab6742b4e166f60
Call Carl <callienicole0419@gmail.com>
2023-03-16 18:39:01 -0500

    added boilerplate and middleware to server.js

--- server.js
+++ server.js
@@ -7,3 +7,18 @@ const exphbs = require('express-handlebars');
 //boilerplpate
 const path = require('path');
 const hbs = exphbs.create({});
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//setting handlebars as default template engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+//app.use(require('./controllers/dish-routes'));
+
+app.listen(PORT, () => {
+    console.log('Server listening on: http://localhost:' + PORT);
+  });
+  
