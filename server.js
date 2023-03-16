0983765bea439b2847d7505f7d75a35f132dacda
Call Carl <callienicole0419@gmail.com>
2023-03-16 17:12:37 -0500

    added package.json and server.js with boilerplate

---
+++ server.js
@@ -0,0 +1,9 @@
+//import express
+const express = require('express');
+
+//import express-handlebars
+const exphbs = require('express-handlebars');
+
+//boilerplpate
+const path = require('path');
+const hbs = exphbs.create({});
